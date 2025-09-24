a1171fd830a0c0c570fa0209fceea21834f8a3f5
EmanuelGarcia <emanuelgarciarios@gmail.com>
2025-09-23 21:53:45 -0500

    Created login view with useState (use client)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,18 +10,13 @@ module.exports = {
       colors: {
         primary: '#1E3A8A',     // Azul oscuro
         secondary: '#FFA024',   // Naranja
-        gray: '#6B7280',        // Gris medio
+        gray: '#DEE1E6',        // Gris medio
         light: '#F5F5F5',       // Gris muy claro
         dark: '#2E2E2E',        // Gris muy oscuro
-        
-        // Variaciones de los colores principales (generadas)
         'primary-light': '#4F6DB3',
         'primary-dark': '#152A65',
-        
         'secondary-light': '#FFB854',
         'secondary-dark': '#E68600',
-        
-        // Colores utilitarios comunes
         error: '#ef4444',
         success: '#22c55e',
         warning: '#f59e0b',
