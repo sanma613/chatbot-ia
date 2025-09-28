e550af7e9f4acdc3b3b724577c5f66d1619e0fe7
EmanuelGarcia <emanuelgarciarios@gmail.com>
2025-09-27 19:47:19 -0500

    Feat:  Created student chat with component

--- frontend/src/app/student/layout.tsx
+++ frontend/src/app/student/layout.tsx
@@ -1,19 +1,14 @@
 
 import Sidebar from '../components/Sidebar';
-import Navbar from '../components/Navbar'
 import React from 'react';
 
 export default function StudentLayout({children,}: {children: React.ReactNode;}){
     return (
         <div className="flex h-screen bg-gray-100 font-sans">
-            <div className="flex flex-1 overflow-hidden">
-                <Sidebar />
-                <main className="flex-1 flex flex-col p-4 lg:p-8 overflow-y-auto">
-                    <div className="flex-1 bg-white rounded-2xl shadow-lg">
-                        {children}
-                    </div>
-                </main>
-            </div>
+            <Sidebar />
+            <main className="flex-1 bg-white">
+                {children}
+            </main>
         </div>
 );
 }
